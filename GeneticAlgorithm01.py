e981f61c7d7ca33173cbd63e59478af3e609fddf
Cezar Azevedo <cezar@grayshirts.com.ar>
2017-09-18 15:25:45 -0300

    Trying to split genes in 2

--- GeneticAlgorithm01.py
+++ GeneticAlgorithm01.py
@@ -22,9 +22,10 @@ class DNA:
                 genes[i] = self.genes[i]
         self.genes = "".join(genes)
     def crossover(self, dna_b):
-        division = random.randint(0, self.gene_size-1)
+        division_a = random.randint(0, self.gene_size - 1)
+        #division_b = random.randint(self.gene_size / 2, self.gene_size -1)
         #division = self.gene_size / 2
-        return DNA(self.gene_size, initial_genes=self.genes[0:division] + dna_b.genes[division:])
+        return DNA(self.gene_size, initial_genes=self.genes[0:division_a] + dna_b.genes[division_a:])
     
     
     def calculate_fitness(self, target):
@@ -38,52 +39,91 @@ class DNA:
 class Population:
     def __init__(self, population_size, target):
         self.population_size = population_size
+        self.population_a_size = population_size / 2
+        self.population_b_size = population_size - self.population_a_size
         self.target = target
-        self.population = []
+        self.population_a = []
+        self.population_b = []
+        for i in xrange(self.population_a_size):
+            self.population_a.append(DNA(len(target)))
+        for i in xrange(self.population_b_size):
+            self.population_b.append(DNA(len(target)))
+
+    def new_population(self, population_size):
+        pop = []
         for i in xrange(population_size):
-            self.population.append(DNA(len(target)))
+            pop.append(DNA(len(target)))
+        return pop
+
     def new_generation(self):
-        pool = []
-        if self.best_fitted().fitness == 100:
+        pool_a = []
+        pool_b = []
+        if self.best_fitted()["a"].fitness == 100 and self.best_fitted()["b"].fitness == 100:
             return True
-        for pop in self.population:
+        for pop in self.population_a:
             for i in xrange(pop.calculate_fitness(self.target)):
-                pool.append(pop)
-        population = []
+                pool_a.append(pop)
+        for pop in self.population_b:
+            for i in xrange(pop.calculate_fitness(self.target)):
+                pool_b.append(pop)
 
-
-        if len(pool) == 0:
-            self = Population(self.population_size, self.target)
+        if len(pool_a) == 0:
+            self.population_a = new_population(self.population_a_size)
             return False
+        if len(pool_b) == 0:
+            self.population_b = new_population(self.population_b_size)
         
+        population_a = []
+        population_b = []
+
         for i in xrange(self.population_size):
-            pop_a = random.choice(pool)
-            pop_b = random.choice(pool)
-            
-            pop_c = pop_a.crossover(pop_b)
-            pop_c.mutate(0.01)
-            
-            population.append(pop_c)
-        self.population = population
+            pop_a1 = random.choice(pool_a)
+            pop_a2 = random.choice(pool_a)
+
+            pop_b1 = random.choice(pool_b)
+            pop_b2 = random.choice(pool_b)
+
+            pop_c1 = pop_a1.crossover(pop_a2)
+            pop_c2 = pop_b1.crossover(pop_b2)
+
+            pop_c1.mutate(0.01)
+            pop_c2.mutate(0.01)
+
+
+            population_a.append(pop_c1)
+            population_b.append(pop_c2)
+
+        self.population_a = population_a
+        self.population_b = population_b
         return False
 
     def best_fitted(self):
-        best = None
-        best_fit = 0
-        for pop in self.population:
-            fit = pop.calculate_fitness(self.target)
-            if fit > best_fit:
-                best_fit = fit
-                best = pop
-        return pop
+        best_a = None
+        best_a_fit = 0
 
-population = Population(150, "Once upon a time.")
+        best_b = None
+        best_b_fit = 0
+
+        for pop in self.population_a:
+            fit = pop.calculate_fitness(self.target)
+            if fit > best_a_fit:
+                best_a_fit = fit
+                best_a = pop
+        for pop in self.population_b:
+            fit = pop.calculate_fitness(self.target)
+            if fit > best_b_fit:
+                best_b_fit = fit
+                best_b = pop
+        return {"a": best_a, "b": best_b}
+
+population = Population(1000, "There was a child named Morty that had a grandpa called Rick.")
 
 while True:
-    for pop in population.population:
-        print ("Phrase: " + str(pop.genes))
-        print ("Fitness: " + str(pop.calculate_fitness(population.target)))
+    print ("Phrase: " + str(population.best_fitted()["a"].genes + population.best_fitted()["b"].genes))
+    print ("FitnessA: " + str(population.best_fitted()["a"].fitness))
+    print ("FitnessB: " + str(population.best_fitted()["b"].fitness))
+
     if (population.new_generation() == True):
         print("DONE.")
-        print(population.best_fitted().genes)
+        print(population.best_fitted()["a"].genes + population.best_fitted()["b"].genes)
         break
